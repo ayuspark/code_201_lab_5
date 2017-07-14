872b50d011510e8de821a97935d223c6a56d155f
ayuspark <lola.luyaowang@gmail.com>
2017-07-14 13:22:36 -0700

    finished problem 2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ For TODO item, be sure to change it to say DONE when you have successfully compl
 // TODO: Write your code here
 function sum(a,b){ //eslint-disable-line
   var result = a + b;
-  var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
   return [result, message]
 }
 
@@ -32,11 +32,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var result = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, message]
 }
 
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
